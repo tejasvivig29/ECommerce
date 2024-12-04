00598afe61fb28ec6fb19f49c085eed4609cd75a
Tejasvi Vig <tejasvi.vig@ordergrid.com>
2024-12-04 19:02:55 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from "react";
+import Loader from './components/loader';
+const Home = lazy(() => import('./pages/home'))
+const Search = lazy(() => import('./pages/search'))
+const Cart = lazy(() => import('./pages/cart'))
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/search" element={<Search />}/>
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
+      </Suspense>
+    </Router>
+  )
+}
+
+export default App;
